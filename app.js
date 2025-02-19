afec8c59976cf9a3d4c772410a307cf8fb718a26
carlos adriano <developer.fsoft@braspress.com.br>
2025-02-19 10:55:37 -0300

    commit 1055

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World - 1053!'));
+app.get('/', (req, res) => res.send('Hello World - 1055!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
